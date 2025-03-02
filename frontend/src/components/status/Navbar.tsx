9af38a72a8b6dc3d85c2d7e43b30207868f36d0d
Ramy Kaddouri <myramkadd@gmail.com>
2025-03-02 02:09:54 -0500

    convert navbar

--- frontend/src/components/status/Navbar.tsx
+++ frontend/src/components/status/Navbar.tsx
@@ -8,18 +8,18 @@ function Navbar() {
     }
 
     return (
-        <div className="navbar">
-            <div className="sign-out-button">
-                <p onClick={() => handleSignout()}>
+        <div className="z-50 flex flex-col w-full pt-4 px-4 justify-start bg-gradient-to-r from-lightblue to-teal">
+            <div className="font-['Rubik'] text-right text-lg font-light cursor-pointer">
+                <p className="hover:underline" onClick={() => handleSignout()}>
                     Sign Out
                 </p>
             </div>
 
-            <img className="site-logo" src="/h4i_umd_logo.png" alt="hack4impact-UMD" />
+            <img className="w-1/4 min-w-40" src="/h4i_umd_logo.png" alt="hack4impact-UMD" />
 
-            <div className="tab-container">
-                <NavLink id="dashboard-link" to="/application">Application</NavLink>
-                <NavLink id="dashboard-link" to="/status">Status</NavLink>
+            <div className="flex gap-4 items-center justify-center w-full">
+                <NavLink id="dashboard-link" className="hover:bg-lightgray active:bg-white font-['Karla'] font-light flex w-1/5 min-w-40 h-14 items-center justify-center text-center text-3xl rounded-t-xl bg-gray text-black select-none transition" to="/application">Application</NavLink>
+                <NavLink id="dashboard-link" className="hover:bg-lightgray active:bg-white font-['Karla'] font-light flex w-1/5 min-w-40 h-14 items-center justify-center text-center text-3xl rounded-t-xl bg-gray text-black select-none transition" to="/status">Status</NavLink>
             </div>
         </div>
     )
@@ -28,16 +28,17 @@ function Navbar() {
 interface NavLinkProps {
     id: string;
     to: string;
+    className: string;
     children: ReactNode
 }
 
 // A special link that includes whether it is currently active in its className
-function NavLink({ to, children, ...props }: NavLinkProps) {
+function NavLink({ to, children, className, ...props }: NavLinkProps) {
     const resolvedPath = useResolvedPath(to);
     const location = useLocation();
     const isActive = location.pathname.startsWith(resolvedPath.pathname);
     return (
-        <Link className={isActive ? "active" : ""} to={to} {...props}>{children}</Link>
+        <Link className={className + (isActive ? " bg-white pointer-events-none" : "")} to={to} {...props}>{children}</Link>
     )
 }
 
