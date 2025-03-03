56033b8b72fdbfee0c330bbeec0691c3ee6d10d5
krishnan mac <inbantholkappian@gmail.com>
2025-03-03 15:58:36 -0500

    completed modal

---
+++ frontend/src/components/reviewer/PrivilegedAccessModal.tsx
@@ -0,0 +1,38 @@
+import { useRef } from "react";
+
+const PrivilegedAccessModal: React.FC = () => {
+
+    const dialogRef = useRef<HTMLDialogElement>(null);
+
+    const closeDialog = () => {
+        dialogRef.current?.close();
+    };
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        
+    };
+
+    return(
+        <dialog ref={dialogRef} open>
+           <main className="flex flex-row h-[50vh] w-[50vw] rounded-xl bg-[#D9D9D9]">
+                 <div className="text-xl text-[#4E4D4D] ml-3 mt-3 cursor-pointer" onClick={closeDialog}>
+                    &larr; Exit 
+                </div>
+
+                <div className="flex flex-col justify-center items-center h-[85%] w-[75%] text-[#4E4D4D] ml-6 mt-9">
+                    <div className="flex flex-col justify-center items-center w-[90%]">
+                      <span className="text-3xl font-bold">Privileged Access</span>
+                      <span className="text-xl font-medium mt-4">Only users with Privileged Access can modify applicant statuses.</span>
+                      <span className="text-xl font-medium mt-4">If you do not have the necessary credentials, please contact your supervisor for assistance.</span>
+                      <form onSubmit={handleSubmit} className="flex flex-col items-center w-full items-center">
+                          <input className="w-full mt-7 p-1.5 bg-white rounded-md outline outline-black" type="password" placeholder="Enter password"/>
+                          <button className="mt-8 text-white w-[60%]" type="submit">Continue</button>
+                      </form>
+                    </div>
+                </div>
+            </main>
+        </dialog>
+    );
+};
+
+export default PrivilegedAccessModal;
