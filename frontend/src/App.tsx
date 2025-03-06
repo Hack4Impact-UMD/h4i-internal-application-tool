fe8dfe4b53ad8187a244a6d4017b33cd2aab02d1
dacevedo-UMD <dacevedo@terpmail.umd.edu>
2025-03-06 02:51:36 -0500

    Initial modifications to add CreateAccount page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import StatusPage from './components/status/StatusPage';
 import DecisionPage from './components/status/DecisionPage';
 import ReviewDashboard from './pages/ReviewDashboard';
 import ApplicantDetails from "./pages/ApplicantDetails"
+import CreateAccount from './pages/CreateAccount';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Route path="/status/decision" element={<DecisionPage />}></Route>
       <Route path="/admin" element={<ReviewDashboard />}></Route>
       <Route path="/admin/applicant/:id" element={<ApplicantDetails />} />
+      <Route path="/createaccount" element={<CreateAccount />}></Route>
     </Routes>
   )
 }
