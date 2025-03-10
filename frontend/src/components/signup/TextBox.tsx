0a4e1d1adbf4360eff79dfbb747a7c992947e3b8
dacevedo-UMD <dacevedo@terpmail.umd.edu>
2025-03-10 02:41:53 -0400

    Made TextBox width/height dynamic, added button enable/disable funcitonality, added invalid states

--- frontend/src/components/signup/TextBox.tsx
+++ frontend/src/components/signup/TextBox.tsx
@@ -1,18 +1,25 @@
 interface TextBoxProps {
-    inputType: String;
-    width: String;
-    height: String;
-    label: String;
-    invalidLabel: String;
+    inputType: string;
+    width: string;
+    height: string;
+    label: string;
+    invalidLabel?: string;
+    onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export default function TextBox({ inputType, width, height, label, invalidLabel }: TextBoxProps ) {
-    const labelClassName = "flex flex-col justify-around w-[" + width + "px] h-[" + height + "px] bg-gray-100 m-1 p-3 border-1 border-transparent rounded-sm hover:border-black"
+export default function TextBox({ inputType, width, height, label, invalidLabel, onChange }: TextBoxProps ) {
+    const borderColor = invalidLabel ? "border-red-500" : "border-transparent";
+    const hover = invalidLabel ? "" : "hover:border-black";
+    var labelClassName = `flex flex-col justify-around bg-gray-100 m-1 p-3 border-1 ${borderColor} rounded-sm ${hover}`;
     
     return (
-        <label className={labelClassName}>
-            <h4 className="font-bold text-xs text-gray-700">{label}</h4>
-            <input type={inputType} className="text-base text-lg font-medium focus:outline-none"></input>
-        </label>
+        // Had to use inline styling to apply dynamic values for width and height as TailWind does not process these values inside square brackets at runtime
+        <div>
+            <label className={labelClassName} style={{ width: `${width}px`, height: `${height}px` }}>
+                <h4 className="font-bold text-xs text-gray-700">{label}</h4>
+                <input type={inputType} className="text-base text-lg font-medium focus:outline-none" onChange={onChange}></input>
+            </label>
+            {invalidLabel && <div style={{ width: `${width}px`}} className="flex justify-end"><p className="text-xs text-red-500 ">{invalidLabel}</p></div>}
+        </div>
     )
 }
--- frontend/src/pages/SignUpCard.tsx
+++ frontend/src/pages/SignUpCard.tsx
@@ -1,6 +1,77 @@
+import { useState } from "react"
 import TextBox from "../components/signup/TextBox"
 
+
 export default function SignUpCard() {
+    const [formData, setFormData] = useState({
+        firstname: "",
+        lastname: "",
+        email: "",
+        password: ""
+    });
+
+    const [formErrors, setFormErrors] = useState({
+        firstname: "",
+        lastname: "",
+        email: "",
+        password: "",
+    });
+    
+
+    // check if all fields are filled
+    const isFormValid = (
+       formData.firstname.trim() !== "" &&
+       formData.lastname.trim() !== "" &&
+       formData.email.trim() !== "" &&
+       formData.password.trim() !== ""
+    );
+
+    // handle TextBox input changes
+    const handleInputChange = (field: string, value: string) => {
+        const updatedData = {
+            ...formData,
+            [field]:value       //adding OR updating a specific field with new value
+        };
+
+        setFormData(updatedData)
+
+        const updatedErrors = {
+            ...formErrors,
+            [field]: ""
+        }
+        
+        setFormErrors(updatedErrors)
+    };
+
+    // using dummy conditionals for now
+    const handleSubmit = () => {
+        let valid = true;
+        let errors = {...formErrors };
+
+        if (formData.firstname.trim() === "") {
+            valid = false;
+            errors.firstname = "Invalid First Name"
+        }
+        if (formData.lastname.trim() === "") {
+            valid = false;
+            errors.lastname = "Invalid Last Name"
+        }
+        if (formData.email.trim() === "" ) {
+            valid = false;
+            errors.email = "Invalid Email"
+        }
+        if (formData.password.length < 6) {
+            valid = false;
+            errors.password = "Invalid Password"
+        }
+
+        setFormErrors(errors)
+
+        if (valid) {
+            // create an account
+        }
+    }
+
     return (
         <div className="flex items-center justify-center min-h-screen bg-white">
             <div className="flex flex-col items-center justify-around w-[430px] h-[700px]">
@@ -13,17 +84,19 @@ export default function SignUpCard() {
                 <div className="flex flex-row">
                     <TextBox 
                         inputType="text"
-                        width="217"
+                        width="175"
                         height="73"
                         label="FIRST NAME"
-                        invalidLabel="TEST"
+                        invalidLabel={formErrors.firstname}
+                        onChange={(e) => handleInputChange("firstname", e.target.value)}
                     />
                     <TextBox 
                         inputType="text"
-                        width="196"
+                        width="238"
                         height="73"
                         label="LAST NAME"
-                        invalidLabel="TEST"
+                        invalidLabel={formErrors.lastname}
+                        onChange={(e) => handleInputChange("lastname", e.target.value)}
                     />
                 </div>
                 <TextBox 
@@ -31,21 +104,31 @@ export default function SignUpCard() {
                     width="421"
                     height="73"
                     label="EMAIL"
-                    invalidLabel="TEST"
+                    invalidLabel={formErrors.email}
+                    onChange={(e) => handleInputChange("email", e.target.value)}
                 />
                 <TextBox 
                     inputType="password"
                     width="421"
                     height="73"
                     label="PASSWORD"
-                    invalidLabel="TEST"
+                    invalidLabel={formErrors.password}
+                    onChange={(e) => handleInputChange("password", e.target.value)}
                 />
-                <button className="w-[421px] h-[73px] bg-[#317FD0] text-white text-lg m-1 rounded-sm focus:border-white">Create account</button>
+                <button 
+                    className={`w-[421px] h-[73px] text-white text-lg m-1 rounded-sm focus:border-white ${
+                        isFormValid ? "bg-[#317FD0] hover:bg-[#265FA1]" : "bg-gray-400"
+                    }`} 
+                    disabled={!isFormValid}
+                    onClick={handleSubmit}
+                >
+                    Create account
+                </button>
                 <div className="w-[421px]">
                     <hr className="w-[421px] text-gray-500 m-0"></hr>
                     <p className="text-gray-500 mt-1">Already have an account? <a href="/" className="text-[#317FD0]">Log In</a></p>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
