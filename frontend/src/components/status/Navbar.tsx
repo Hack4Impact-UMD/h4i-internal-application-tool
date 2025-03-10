86627bd5e729449d37cbf767e735d8d8fdfa6a1c
dewang47 <dephs28@gmail.com>
2025-03-10 18:27:59 -0400

    tweak sign-in button and logo styles

--- frontend/src/components/status/Navbar.tsx
+++ frontend/src/components/status/Navbar.tsx
@@ -12,12 +12,12 @@ function Navbar({ isSignedIn }: NavbarProps) {
     }
 
     return (
-        <div className="z-50 flex flex-row w-full py-4 px-8 justify-start bg-gray h-20">
+        <div className="z-50 flex flex-row w-full py-4 px-8 justify-start bg-lightgray h-20">
             <div className="flex flex-2 ml-16 items-center">
-                <img className="w-48" src="/public/h4i_logo.png" alt="hack4impact-UMD" />
+                <img className="w-36" src="/public/h4i_logo.png" alt="hack4impact-UMD" />
             </div>
             {isSignedIn && (
-                <div className="font-['Rubik'] text-right text-xl font-medium cursor-pointer">
+                <div className="font-['Rubik'] text-right text-m font-extralight cursor-pointer flex flex-col justify-center pr-[7%]">
                 <p className="hover:underline" onClick={() => handleSignout()}>
                     Sign Out
                 </p>
