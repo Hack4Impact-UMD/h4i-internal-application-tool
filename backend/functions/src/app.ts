fd0f07fbb775956ade50da90183fd0349f2b70f9
Ramy Kaddouri <myramkadd@gmail.com>
2025-03-12 16:44:14 -0400

    update firebase rules for now to be more secure

--- backend/functions/src/app.ts
+++ backend/functions/src/app.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import helloRouter from "./routes/hello";
+import authRouter from "./routes/auth";
+import bodyParser from "body-parser";
 
 const app = express();
+app.use(bodyParser);
 
 app.use("/hello", helloRouter);
+app.use("/auth", authRouter);
 
 export default app;
 
