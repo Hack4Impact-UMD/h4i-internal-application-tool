5a01bbbef3a01b0fc142431f189bf5c9dd624e88
Ramy Kaddouri <myramkadd@gmail.com>
2025-03-16 15:43:25 -0400

    navbar improvements

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -8,17 +8,20 @@ function Navbar() {
     }
 
     return (
-        <div className="z-50 flex flex-row w-full py-4 px-8 justify-start bg-lightgray h-20">
-            <div className="flex flex-2 ml-16 items-center">
-                <img className="w-36" src="/h4i_logo.png" alt="hack4impact-UMD" />
-            </div>
-            {!isLoading && isAuthed && (
-                <div className="font-['Rubik'] text-right text-m font-extralight cursor-pointer flex flex-col justify-center pr-[7%]">
-                    <p className="hover:underline" onClick={() => handleSignout()}>
-                        Sign Out
-                    </p>
+        <div className="z-50 flex flex-row w-full py-4 px-8 justify-center items-center bg-lightgray h-20">
+            <div className="flex flex-row h-full max-w-5xl w-full">
+                <div className="flex flex-2 items-center">
+                    <img className="w-36" src="/h4i_logo.png" alt="hack4impact-UMD" />
                 </div>
-            )}
+                {!isLoading && isAuthed && (
+                    <div className="font-['Rubik'] text-right text-m font-extralight cursor-pointer flex flex-col justify-center ">
+                        <p className="hover:underline" onClick={() => handleSignout()}>
+                            Sign Out
+                        </p>
+                    </div>
+                )}
+
+            </div>
         </div>
     )
 }
