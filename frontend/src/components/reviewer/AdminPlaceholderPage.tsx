a191f15148f417219f100efe3bdc06f2a4fa4fcc
Ramy Kaddouri <myramkadd@gmail.com>
2025-03-18 00:54:18 -0400

    add files

---
+++ frontend/src/components/reviewer/AdminPlaceholderPage.tsx
@@ -0,0 +1,15 @@
+import { useApplicants } from "../../hooks/useApplicants";
+
+export default function AdminPlaceholderPage() {
+  const { data: applicants, isLoading, error } = useApplicants()
+
+  if (isLoading) return <p>Loading...</p>
+  if (error) return <p>Error: {error.message}</p>
+
+  return <div className="p-4">
+    <h1>All Applicants: </h1>
+    <ul>
+      {applicants?.map(applicant => <li>{applicant.firstName} (ID: {applicant.id})</li>)}
+    </ul>
+  </div>
+}
