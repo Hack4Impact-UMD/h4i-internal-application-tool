6a7a4228a0b139abdf7e489ea9ce37ec82c23d1f
dacevedo-UMD <dacevedo@terpmail.umd.edu>
2025-03-28 19:08:42 -0400

    add custom colors to all components, fix RestPass to include proper main div

--- frontend/src/components/TextBox.tsx
+++ frontend/src/components/TextBox.tsx
@@ -1,23 +1,37 @@
 interface TextBoxProps {
-    inputType: string;
-    className: string;
-    label: string;
-    invalidLabel?: string;
-    onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  inputType: string;
+  className: string;
+  label: string;
+  invalidLabel?: string;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export default function TextBox({ inputType, label, className, invalidLabel, onChange }: TextBoxProps) {
-    const borderColor = invalidLabel ? "border-red-500" : "border-transparent";
-    const hover = invalidLabel ? "" : "hover:border-black";
-    const labelClassName = `flex flex-col justify-around bg-[#F3F3F3] m-1 p-3 border-1 ${borderColor} rounded-sm ${hover}`;
+export default function TextBox({
+  inputType,
+  label,
+  className,
+  invalidLabel,
+  onChange,
+}: TextBoxProps) {
+  const borderColor = invalidLabel ? "border-red" : "border-transparent";
+  const hover = invalidLabel ? "" : "hover:border-black";
+  const labelClassName = `flex flex-col justify-around bg-lightgray m-1 p-3 border-1 ${borderColor} rounded-sm ${hover}`;
 
-    return (
-        <div className={className}>
-            <label className={labelClassName}>
-                <h4 className="font-bold text-xs text-gray-700">{label}</h4>
-                <input type={inputType} className="text-lg font-medium focus:outline-none" onChange={onChange}></input>
-            </label>
-            {invalidLabel && <div className="flex justify-end p-1"><p className="text-xs text-red-500 ">{invalidLabel}</p></div>}
+  return (
+    <div className={className}>
+      <label className={labelClassName}>
+        <h4 className="font-bold text-xs text-darkgray">{label}</h4>
+        <input
+          type={inputType}
+          className="text-lg font-medium focus:outline-none"
+          onChange={onChange}
+        ></input>
+      </label>
+      {invalidLabel && (
+        <div className="flex justify-end p-1">
+          <p className="text-xs text-red">{invalidLabel}</p>
         </div>
-    )
+      )}
+    </div>
+  );
 }
--- frontend/src/pages/ForgotPass/ForgotPassCard.tsx
+++ frontend/src/pages/ForgotPass/ForgotPassCard.tsx
@@ -1,91 +1,92 @@
-import { useState } from "react"
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { validEmail } from "../../utils/verification";
-import TextBox from "../../components/TextBox"
+import TextBox from "../../components/TextBox";
 import Button from "../../components/Button";
 
 export default function ForgotPassCard() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const [formData, setFormData] = useState({
-        email: ""
-    });
+  const [formData, setFormData] = useState({
+    email: "",
+  });
 
-    const [formErrors, setFormErrors] = useState({
-        email: ""
-    });
+  const [formErrors, setFormErrors] = useState({
+    email: "",
+  });
 
+  // check if all fields are filled
+  const isFormValid = formData.email.trim() !== "";
 
-    // check if all fields are filled
-    const isFormValid = (
-        formData.email.trim() !== ""
-    );
-
-    // handle TextBox input changes
-    const handleInputChange = (field: string, value: string) => {
-        const updatedData = {
-            ...formData,
-            [field]: value       //adding OR updating a specific field with new value
-        };
-
-        setFormData(updatedData)
-
-        const updatedErrors = {
-            ...formErrors,
-            [field]: ""
-        }
-
-        setFormErrors(updatedErrors)
+  // handle TextBox input changes
+  const handleInputChange = (field: string, value: string) => {
+    const updatedData = {
+      ...formData,
+      [field]: value, //adding OR updating a specific field with new value
     };
 
-    // using dummy conditionals for now
-    const handleSubmit = () => {
-        let valid = true;
-        const errors = { ...formErrors };
+    setFormData(updatedData);
 
-        if (!validEmail(formData.email)) {
-            valid = false;
-            errors.email = "Enter a valid terpmail address"
-        }
+    const updatedErrors = {
+      ...formErrors,
+      [field]: "",
+    };
 
-        setFormErrors(errors)
+    setFormErrors(updatedErrors);
+  };
 
-        if (valid) {
-            navigate("/resetpassword")
-        }
+  // using dummy conditionals for now
+  const handleSubmit = () => {
+    let valid = true;
+    const errors = { ...formErrors };
+
+    if (!validEmail(formData.email)) {
+      valid = false;
+      errors.email = "Enter a valid terpmail address";
     }
 
-    return (
-        <form
-            onSubmit={(e) => {
-                e.preventDefault()
-                e.stopPropagation()
-                handleSubmit()
-            }}
-            className="flex flex-col items-center justify-around p-4 w-full max-w-[430px] h-[500px]">
-            <img src="h4i-logo.png" alt="h4i logo" className="h-[105px] w-[105px]" />
-            <div className="flex flex-col items-center text-center justify-around w-[305px] h-[105px]">
-                <h1 className="text-3xl font-bold">Forgot Password?</h1>
-                <h3 className="text-lg text-gray-500">No worries! You will recieve a code in your email to reset your account</h3>
-            </div>
-            <TextBox
-                inputType="text"
-                className="w-full"
-                label="EMAIL"
-                invalidLabel={formErrors.email}
-                onChange={(e) => handleInputChange("email", e.target.value)}
-            />
-            <div className="w-full">
-                <Button
-                    className="w-full h-[73px]"
-                    label="Send code"
-                    enabled={isFormValid}
-                    type="submit"
-                />
-            </div>
-            <div className="w-full">
-                <hr className="w-full text-gray-500 m-0"></hr>
-            </div>
-        </form>
-    );
-};
+    setFormErrors(errors);
+
+    if (valid) {
+      navigate("/resetpassword");
+    }
+  };
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        handleSubmit();
+      }}
+      className="flex flex-col items-center justify-around p-4 w-full max-w-[430px] h-[500px]"
+    >
+      <img src="h4i-logo.png" alt="h4i logo" className="h-[105px] w-[105px]" />
+      <div className="flex flex-col items-center text-center justify-around w-[305px] h-[105px]">
+        <h1 className="text-3xl font-bold">Forgot Password?</h1>
+        <h3 className="text-lg text-darkgray">
+          No worries! You will recieve a code in your email to reset your
+          account
+        </h3>
+      </div>
+      <TextBox
+        inputType="text"
+        className="w-full"
+        label="EMAIL"
+        invalidLabel={formErrors.email}
+        onChange={(e) => handleInputChange("email", e.target.value)}
+      />
+      <div className="w-full">
+        <Button
+          className="w-full h-[73px]"
+          label="Send code"
+          enabled={isFormValid}
+          type="submit"
+        />
+      </div>
+      <div className="w-full">
+        <hr className="w-full text-darkgray m-0"></hr>
+      </div>
+    </form>
+  );
+}
