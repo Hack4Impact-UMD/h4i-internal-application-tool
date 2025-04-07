ec55bd7c77d153b808961162a6f4e4d0b14cd970
Ramy Kaddouri <myramkadd@gmail.com>
2025-04-07 18:01:55 -0400

    disable password verification for now

--- frontend/src/utils/verification.ts
+++ frontend/src/utils/verification.ts
@@ -1,5 +1,6 @@
 const emailRegExp = new RegExp("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$")
 const passwordRegExp = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{8,}$/
+//TODO: this doesn't seem to be working!
 // why doesnt this work in RegExp()? ^
 const codeRegExp = new RegExp("^[A-Z0-9]{6}$")
 
@@ -8,7 +9,8 @@ export function validEmail(email: string): boolean {
 }
 
 export function validPassword(password: string): boolean {
-  return passwordRegExp.test(password)
+  // return passwordRegExp.test(password)
+  return password.length >= 6;
 }
 
 export function validCode(code: string): boolean {
