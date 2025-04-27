3b236bd853679b290fab99851c43f155571bef3e
krishnan mac <inbantholkappian@gmail.com>
2025-04-26 22:29:27 -0400

    added disabled component styles, added section name to "next" button, changed "next" button label style to match figma

--- frontend/src/components/form/MultiSelectGroup.tsx
+++ frontend/src/components/form/MultiSelectGroup.tsx
@@ -3,53 +3,64 @@ import OptionButton from "./OptionButton";
 import { twMerge } from "tailwind-merge";
 
 interface MultiSelectGroupProps {
-    question: string,
-    isRequired?: boolean,
-    label?: string,
-    value?: string[],
-    options: string[],
-    onOptionSelect: (selected: string[]) => void,
-    className?: string
+  question: string;
+  isRequired?: boolean;
+  label?: string;
+  value?: string[];
+  options: string[];
+  onOptionSelect: (selected: string[]) => void;
+  className?: string;
+  disabled?: boolean;
 }
 
-const MultiSelectGroup: React.FC<MultiSelectGroupProps> = ({ question, isRequired, label, value, options, onOptionSelect, className = "" }) => {
-    const [selectedOptions, setSelectedOptions] = useState<string[]>(value || []);
+const MultiSelectGroup: React.FC<MultiSelectGroupProps> = ({
+  question,
+  isRequired,
+  label,
+  value,
+  options,
+  onOptionSelect,
+  className = "",
+  disabled,
+}) => {
+  const [selectedOptions, setSelectedOptions] = useState<string[]>(value || []);
 
-    useEffect(() => {
-        setSelectedOptions(value || []);
-    });
+  useEffect(() => {
+    setSelectedOptions(value || []);
+  });
 
-    const handleSelectClick = (optionName: string) => {
-        const updatedSelections = selectedOptions.includes(optionName)
-            ? selectedOptions.filter((option) => option !== optionName)
-            : [...selectedOptions, optionName];
+  const handleSelectClick = (optionName: string) => {
+    const updatedSelections = selectedOptions.includes(optionName)
+      ? selectedOptions.filter((option) => option !== optionName)
+      : [...selectedOptions, optionName];
 
-        setSelectedOptions(updatedSelections);
-        onOptionSelect(updatedSelections);
-    };
+    setSelectedOptions(updatedSelections);
+    onOptionSelect(updatedSelections);
+  };
 
-    return (
-        <main className={twMerge("flex flex-col min-w-60", className)}>
+  return (
+    <main className={twMerge("flex flex-col min-w-60", className)}>
+      <span className="text-xl font-normal">
+        {question}{" "}
+        {!isRequired && <span className="font-light text-xs"> (Optional)</span>}
+      </span>
 
-            <span className="text-xl font-normal">
-                {question} {!isRequired && <span className="font-light text-xs"> (Optional)</span>}
-            </span>
+      <span className="mb-2.5 text-xs font-light">{label}</span>
 
-            <span className="mb-2.5 text-xs font-light">{label}</span>
-
-            <div className="flex flex-wrap gap-1 mt-2">
-                {options.map((option) => (
-                    <OptionButton
-                        key={option}
-                        optionName={option}
-                        buttonType="multiSelect"
-                        isSelected={selectedOptions.includes(option)}
-                        onClick={() => handleSelectClick(option)}
-                    />
-                ))}
-            </div>
-        </main>
-    );
-}
+      <div className="flex flex-wrap gap-1 mt-2">
+        {options.map((option) => (
+          <OptionButton
+            key={option}
+            optionName={option}
+            buttonType="multiSelect"
+            isSelected={selectedOptions.includes(option)}
+            onClick={() => handleSelectClick(option)}
+            disabled={disabled}
+          />
+        ))}
+      </div>
+    </main>
+  );
+};
 
 export default MultiSelectGroup;
