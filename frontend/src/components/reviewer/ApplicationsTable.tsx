7aa3bcce9da3935c50467f11033ab1fb41149131
Ramy Kaddouri <myramkadd@gmail.com>
2025-06-16 21:42:36 -0400

    fix status page bug and improve overview page when applied

--- frontend/src/components/reviewer/ApplicationsTable.tsx
+++ frontend/src/components/reviewer/ApplicationsTable.tsx
@@ -1,3 +1,9 @@
-export default function ApplicationsTable() {
+import { ReviewerAssignment } from "@/types/types"
+
+type ReviewerApplicationsTableProps = {
+	assignments: ReviewerAssignment[]
+}
+
+export default function ReviewerApplicationsTable({ assignments }: ReviewerApplicationsTableProps) {
 	return <p>Applications table goes here</p>
 }
---
+++ frontend/src/components/reviewer/ReviewerApplicationsTable.tsx
@@ -0,0 +1,3 @@
+export default function ReviewerApplicationsTable() {
+	return <p>Applications table goes here</p>
+}
