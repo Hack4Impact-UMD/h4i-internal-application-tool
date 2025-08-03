7bc71c5c3b2c6975687317d4ef25cab73e574a95
lanceu0128 <lanceu0128@gmail.com>
2025-08-02 23:54:24 -0700

    add suggestions/warnings to backup application responses

---
+++ frontend/src/components/form/DataWarningDialog.tsx
@@ -0,0 +1,53 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "../ui/alert-dialog";
+import { useEffect, useState } from "react";
+
+type DataWarningDialogProps = {
+  open: boolean;
+  onSubmit: () => void;
+};
+
+export default function DataWarningDialog({
+  open,
+  onSubmit,
+}: DataWarningDialogProps) {
+  const [ buttonDisabled, setButtonDisabled ] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setButtonDisabled(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  });
+
+  return (
+    <AlertDialog open={open}>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+           ⚠️ Warning
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            Welcome to the first live run of Hack4Impact-UMD's Application Portal! This app is designed to autosave your responses. However, as this is our initial launch, there may be unforeseen issues. Backup your responses elsewhere (e.g. in Google Docs)... just in case!
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogAction
+            onClick={() => onSubmit()}
+            disabled={buttonDisabled}
+          >
+            {buttonDisabled ? "Read the message" : "Take me to the app"}
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
