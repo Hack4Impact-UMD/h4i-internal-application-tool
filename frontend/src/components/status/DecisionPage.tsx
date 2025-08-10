94d6296a1a742b554d765c754add5e58bc6e748b
lanceu0128 <lanceu0128@gmail.com>
2025-08-10 13:59:04 -0700

    add confetti to decision page

--- frontend/src/components/status/DecisionPage.tsx
+++ frontend/src/components/status/DecisionPage.tsx
@@ -8,6 +8,7 @@ import FormMarkdown from "../form/FormMarkdown";
 import ErrorPage from "@/pages/ErrorPage";
 import NotFoundPage from "@/pages/NotFoundPage";
 import { useApplicationFormForResponseId } from "@/hooks/useApplicationForm";
+import ConfettiExplosion from 'react-confetti-explosion';
 
 function DecisionPage() {
   const { user } = useAuth();
@@ -84,23 +85,34 @@ function DecisionPage() {
     : form.decisionLetter?.[appStatus.status];
 
   return (
-    <div className="mt-5 mx-auto max-w-5xl w-full px-5 py-5 font-sans leading-relaxed">
-      <div className="flex gap-2 flex-col sm:flex-row items-start justify-between mb-5">
-        <div className="flex flex-col">
-          <h2 className="text-blue text-2xl">
-            Your {displayApplicantRoleName(role as ApplicantRole).slice(2)}{" "}
-            application for <br></br> Hack4Impact-UMD{" "}
-          </h2>
+    <>
+      <div className="mt-5 mx-auto max-w-5xl w-full px-5 py-5 font-sans leading-relaxed">
+        <div className="flex gap-2 flex-col sm:flex-row items-start justify-between mb-5">
+          <div className="flex flex-col">
+            <h2 className="text-blue text-2xl">
+              Your {displayApplicantRoleName(role as ApplicantRole).slice(2)}{" "}
+              application for <br></br> Hack4Impact-UMD{" "}
+            </h2>
+          </div>
+        </div>
+        <div className="font-[Karla] text-sm font-normal leading-tight text-justify [text-justify:inter-word]">
+          <p>
+            Dear {user.firstName} {user.lastName},
+          </p>
+          <br></br>
+          {appStatus.status === ReviewStatus.Accepted && 
+            <ConfettiExplosion 
+              className="justify-self-center self-start"
+              force={0.8}
+              duration={3000}
+              particleCount={250}
+              width={1600}
+            />
+          }
+          <FormMarkdown>{decisionLetterText}</FormMarkdown>
         </div>
       </div>
-      <div className="font-[Karla] text-sm font-normal leading-tight text-justify [text-justify:inter-word]">
-        <p>
-          Dear {user.firstName} {user.lastName},
-        </p>
-        <br></br>
-        <FormMarkdown>{decisionLetterText}</FormMarkdown>
-      </div>
-    </div>
+    </>
   );
 }
 
