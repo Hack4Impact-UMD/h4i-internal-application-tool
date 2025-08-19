af84ef59a0877f789addc42900e1e7db3cf391d5
Lance <81938513+lanceu0128@users.noreply.github.com>
2025-08-19 07:06:26 -0700

    date submitted column (#56)
    * workign date submitted column for under review and qualified dashboards; new date package and utility function
    
    * change text to be date submitted since all apps in under review are already submitted
    
    * fmt
    
    ---------
    
    Co-authored-by: Ramy Kaddouri <myramkadd@gmail.com>

---
+++ frontend/src/utils/dates.ts
@@ -0,0 +1,6 @@
+import { Timestamp } from "firebase/firestore";
+import { format } from "date-fns";
+
+export function displayTimestamp(timestamp: Timestamp) {
+  return format(timestamp.toDate(), "M/d h:mmaaa");
+}
