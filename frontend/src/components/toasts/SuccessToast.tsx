041ade1825274e8f88cc52b44b98537c7786d499
lanceu0128 <lanceu0128@gmail.com>
2025-08-18 23:15:47 -0700

    add close button and shorter autoClose to success toast

--- frontend/src/components/toasts/SuccessToast.tsx
+++ frontend/src/components/toasts/SuccessToast.tsx
@@ -1,19 +1,30 @@
-import { CheckIcon } from "lucide-react";
 import { toast } from "react-toastify";
 
-export default function SuccessToast({ message }: { message: string }) {
+type SuccessToastProps = {
+  message: string;
+  closeToast?: () => void;
+};
+
+export default function SuccessToast({ message, closeToast }: SuccessToastProps) {
   return (
     <div className="bg-green-100 text-black border border-green-300 rounded-xl shadow-lg p-4 flex items-start gap-2 max-w-md z-50">
-      <CheckIcon></CheckIcon>
+      <span>✅</span>
       <div className="flex-1 text-sm">{message}</div>
+      <span
+        onClick={closeToast}
+        className="cursor-pointer"
+      >
+        X
+      </span>
     </div>
   );
 }
 
-export function throwSuccessToast(message: string) {
+export function throwSuccessToast(message: string, autoClose: number = 2000) {
   toast.success(<SuccessToast message={message} />, {
     closeButton: false,
     hideProgressBar: true,
     icon: false,
+    autoClose: autoClose,
   });
 }
