0866303a114ff0acf05ffe806600d552d194139d
Ramy Kaddouri <myramkadd@gmail.com>
2025-12-18 21:12:26 -0500

    add inactive user flag + hook up user table

--- backend/functions/src/models/user.ts
+++ backend/functions/src/models/user.ts
@@ -12,6 +12,7 @@ export type UserProfile = {
   dateCreated: Timestamp;
   activeApplications?: string[];
   inactiveApplications?: string[]
+  inactive?: boolean
 }
 
 export const userRegisterFormSchema = z.object({
@@ -19,11 +20,11 @@ export const userRegisterFormSchema = z.object({
   firstName: z.string().nonempty("First name can't be empty"),
   lastName: z.string().nonempty("Last name can't be empty"),
   password: z.string()
-  .min(8, "Password must be at least 8 characters")
-  .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
-  .regex(/[a-z]/, "Password must contain at least one lowercase letter")
-  .regex(/[0-9]/, "Password must contain at least one number")
-  .regex(/[^A-Za-z0-9]/, "Password must contain at least one special character"),
+    .min(8, "Password must be at least 8 characters")
+    .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+    .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+    .regex(/[0-9]/, "Password must contain at least one number")
+    .regex(/[^A-Za-z0-9]/, "Password must contain at least one special character"),
 
 })
 
