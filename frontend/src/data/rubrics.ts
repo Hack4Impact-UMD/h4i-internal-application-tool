1d64eba8a11d0706539c65aa895a8ea3df757e9c
Ramy Kaddouri <myramkadd@gmail.com>
2025-12-25 11:32:00 -0500

    update rubric form id

--- frontend/src/data/rubrics.ts
+++ frontend/src/data/rubrics.ts
@@ -1,7 +1,7 @@
 import { ApplicantRole } from "../types/formBuilderTypes";
 import { RoleReviewRubric } from "../types/types";
 
-const FORM_ID = "h4i-fall-2025-form-final";
+const FORM_ID = "h4i-spring-2026-form-final";
 
 export const APPLICATION_RUBRICS: RoleReviewRubric[] = [
   {
